ddb163deaff178e7ff3dab9da1830a8d391ea95d
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2022-06-04 06:15:35 +0200

    fixed sdk synx error

--- src/helpers/connect.js
+++ src/helpers/connect.js
@@ -1,25 +1,3 @@
-
-import { Sdk, MetaMaskWalletProvider } from 'etherspot';
-
-
-async function main() {
-  if (!MetaMaskWalletProvider.detect()) {
-    console.log('MetaMask not detected');
-    return;
-  }
-
-  const walletProvider = await MetaMaskWalletProvider.connect();
-
-  const sdk = new Sdk(walletProvider, {
-    projectKey: '0x3eb8ad8d046491c08bc529f0f267365dafe04577d1bed4f3080e62f1c061642e',
-  });
-
-  console.info('SDK created');
-}
-
-main().catch(console.error);
-
-
 import { Sdk, MetaMaskWalletProvider } from 'etherspot';
 
 async function main() {
@@ -38,6 +16,7 @@ async function main() {
 }
 
 main().catch(console.error);
+
 
 export async function fetchData() {
     return new Promise((resolve, reject) => {
