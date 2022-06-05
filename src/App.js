a47e926d4c0b43ba7ce1fb6fee7f94afb2c2a949
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2022-06-05 08:53:36 +0200

    error in page load

--- src/App.js
+++ src/App.js
@@ -1,29 +1,111 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { connectMetamask } from "./helpers";
+import { fetchData } from "./helpers";
+const { DEFAULT_NETWORK, BANNER } = require("./.secret.json");
 
-import 'etherspot-react-wallet/dist/index.css';
-import { useState, useEffect } from "react";
 
 
-function App() {
-  const [address, setAddress] = useState("0x");
-  useEffect(() => {
-    
- 
-    
-   
-  }, []);
 
- 
+const MyApp = ({ Component, pageProps }) => {
+  const [, setNetwork] = useState("");
+  const [correctNetwork, setCorrectNetwork] = useState(false);
+  const [connectted, setConnectted] = useState(false);
+  const [hasMetamask, setHasMetamask] = useState(false);
   
+
+  function App() {
+    const [address, setAddress] = useState("0x");
+    useEffect(() => {
+      const promise = fetchData();
+      promise.then((data) => {
+        setAddress(data.user);
+      });
+    }, []);
+
+  function accountChanged(_accounts) {
+    // Time to reload your interface with accounts[0]!
+    setAddress(_accounts.length >= 1 ? _accounts[0] : "no account");
+  }
+
+  function networkChanged(_chainId) {
+    // Time to reload your interface with the new networkId
+    setNetwork(_chainId);
+    setCorrectNetwork(_chainId === DEFAULT_NETWORK ? true : false);
+  }
+
+
+
+  const connect = async () => {
+    const { address, network } = await connectMetamask(
+      accountChanged,
+      networkChanged
+    );
+    if (address === "no account" || address === "no metamask") {
+    } else {
+      setNetwork(network);
+      setAddress(address);
+      setConnectted(true);
+      setHasMetamask(true);
+    }
+    setCorrectNetwork(network === DEFAULT_NETWORK ? true : false);
+    if (correctNetwork) {
+      console.log("correct network", network);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-   
-      <p>{address}</p>
+
+      {!correctNetwork && `${BANNER} `}
+      {!connectted && hasMetamask && (
+                  <div>
+                    <button
+                      class="btn btn-primary"
+                      type="button"
+                      onClick={connect}
+                    >
+                      connect 
+                    </button>
+                  </div>
+                )}
+              {connectted && correctNetwork && (
+                  <div>
+                    <p>{address}</p>
+                  </div>
+                )}
+
+
+                {!hasMetamask && (
+                  <>
+                    <a
+                      href="/"
+                      class="btn btn-primary"
+                    >
+                      connect
+                    </a>
+                    <div>
+                      <div >
+                        <p>Please install Metamask</p>
+                        <div >
+                          <a
+                            href="https://metamask.io/"
+                            
+                          >
+                            Metamask 
+                          </a>
+                          <a href="/" class="btn">
+                            Cancel
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
       </header>
     </div>
- 
   );
 }
-
-export default App;
+}
+export default MyApp;
--- src/helpers/connect.js
+++ src/helpers/connect.js
@@ -1,80 +1,39 @@
-import { Sdk, MetaMaskWalletProvider } from 'etherspot';
-import detectEthereumProvider from '@metamask/detect-provider';
+import { ethers } from "ethers";
 
-import { ethers } from 'ethers'
-import { useState } from 'react';
+export var provider;
+export var signer;
 
-
-
-
-export const ConnectComponent = ({ text }: Props) => {
-  const ethereum = window.ethereum
-const [account, setAccount] = useState('👻👻👻👻👻👻👻👻👻👻👻👻');
-// install metamask
-const iMetamask = async () => {
-  const provider = await detectEthereumProvider();
-  if (provider) {
-    console.log(provider)
-    startApp(provider); 
-  } else {
-    console.log('Please install MetaMask!');
+export const connectMetamask = async (
+  accountChangedCallback,
+  networkChangedCallback
+) => {
+  if (typeof window !== "undefined") {
+    if (typeof window.ethereum === "undefined") {
+      console.log("MetaMask NOT installed!");
+      return { address: "no metamask", network: "no network" };
+    } else {
+      window.ethereum.on("accountsChanged", accountChangedCallback);
+      window.ethereum.on("chainChanged", networkChangedCallback);
+      provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+      // Prompt user for account connections
+      await provider.send("eth_requestAccounts", []);
+      signer = provider.getSigner();
+      const address = await signer.getAddress();
+      const { chainId } = await provider.getNetwork();
+      const chain = "0x" + chainId.toString(16);
+      return { address: address, network: chain };
+    }
   }
-}
-
-const startApp = async (provider: ant) => {
-  const library = new ethers.providers.Web3Provider(provider)
-  const signer = library.getSigner()
-  try {
-    const addr = await signer.getAddress()
-    console.log(addr)
-    console.log('signer ', signer)
-    setAccount(addr)
-  }
-  catch (ex) {
-    console.log(ex);
-  }
-}
+};
 
 
-
-
-async function main() {
-  if (!MetaMaskWalletProvider.detect()) {
-    console.log('MetaMask not detected');
-    return;
-  }
-
-  const walletProvider = await MetaMaskWalletProvider.connect();
-
-  const sdk = new Sdk(walletProvider, {
-    projectKey: '0x3eb8ad8d046491c08bc529f0f267365dafe04577d1bed4f3080e62f1c061642e',
+export async function fetchData() {
+  return new Promise((resolve, reject) => {
+    try {
+      const data = { user: "samar" };
+      resolve(data);
+    } catch (error) {
+      reject("error");
+    }
   });
-
-  console.info('SDK created');
-}
-main().catch(console.error);
-
-
-
-
-
-
-
-const connectMetamask = async () => {
-  console.log('Start metamask ')
-  const accounts = await ethereum.request({ method: 'eth_request' });
-  const account = accounts[0];
-  console.log('account ', account)
-  setAccount(account)
-}
-
-
- function fetchData() {
-  return <div>
-   
-    <button type="button" onClick={connectMetamask}>
-    connect 
-    </button>
-  </div>
-  }
 }
